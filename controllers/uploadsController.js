d2fc73d35e72fbe05b982558b5d94a723b21bfb3
ashutosh1506 <guptaashutosh502@gmail.com>
2024-07-25 15:18:27 +0530

    Added Cloudinary

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,11 @@
 const { StatusCodes } = require("http-status-codes");
 const path = require("path");
 const CustomError = require("../errors");
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
 
-const uploadProductImage = async (req, res) => {
+//This is for uploading images locally
+const uploadProductImageLocal = async (req, res) => {
   //check if file exists
   if (!req.files) {
     throw new CustomError.BadRequestError("No File Uploaded");
@@ -30,6 +33,20 @@ const uploadProductImage = async (req, res) => {
   return res
     .status(StatusCodes.OK)
     .json({ image: { src: `/uploads/${productImage.name}` } });
+};
+
+//This is for uploading images on cloudinary
+
+const uploadProductImage = async (req, res) => {
+  const result = await cloudinary.uploader.upload(
+    req.files.image.tempFilePath,
+    {
+      use_filename: true,
+      folder: "file-upload",
+    }
+  );
+  fs.unlinkSync(req.files.image.tempFilePath);
+  return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
 };
 module.exports = {
   uploadProductImage,
