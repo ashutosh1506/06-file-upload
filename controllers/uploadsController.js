42bbd09cd0031cf5de8f6cc1d02ba825f5962c56
ashutosh1506 <guptaashutosh502@gmail.com>
2024-07-25 14:47:03 +0530

    Update uploadsController.js
    Additional checks for image

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -1,8 +1,27 @@
 const { StatusCodes } = require("http-status-codes");
 const path = require("path");
+const CustomError = require("../errors");
 
 const uploadProductImage = async (req, res) => {
-  let productImage = req.files.image;
+  //check if file exists
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No File Uploaded");
+  }
+
+  const productImage = req.files.image;
+
+  //check format
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError("Please Provide Image");
+  }
+
+  //check size
+  const maxSize = 1024 * 1024; //1mb
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      "Please Upload image with size less than 1kb"
+    );
+  }
   const imagePath = path.join(
     __dirname,
     "../public/uploads/" + `${productImage.name}`
